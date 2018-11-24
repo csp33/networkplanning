4ca0d5b06aef28a872271d82f971b9dc6d63cc40
Carlos <karlossanpa@gmail.com>
2018-11-24 04:10:02 +0100

    Upgraded draw_graph() function

--- tools.py
+++ tools.py
@@ -11,36 +11,56 @@ import matplotlib.pyplot as plt
 import pydot
 from pulp import *
 
+
 def create_graph():
     G = nx.DiGraph()
 
-    # Add the edges
+    # Add the nodes
+    G.add_node('A', pos=(0, 2))
+    G.add_node('B', pos=(2, 3))
+    G.add_node('C', pos=(2, 1))
+    G.add_node('D', pos=(3.5, 2))
+    G.add_node('E', pos=(5, 3))
+    G.add_node('F', pos=(5, 1))
+    G.add_node('G', pos=(7, 2))
 
-    G.add_edge('A', 'B', cost=20, capacity=20, label='(20,20)', color='r')
-    G.add_edge('A', 'C', cost=7, capacity=15, label='(7,15)', color='b')
-    G.add_edge('B', 'E', cost=16, capacity=21, label='(16,21)', color='b')
-    G.add_edge('B', 'D', cost=4, capacity=4, label='(4,4)', color='b')
-    G.add_edge('C', 'D', cost=4, capacity=4, label='(4,4)', color='b')
-    G.add_edge('C', 'F', cost=3, capacity=9, label='(3,9)', color='b')
-    G.add_edge('D', 'E', cost=2, capacity=2, label='(2,2)', color='b')
-    G.add_edge('D', 'F', cost=6, capacity=10, label='(6,10)', color='b')
-    G.add_edge('E', 'G', cost=18, capacity=24, label='(18,24)', color='b')
-    G.add_edge('F', 'G', cost=9, capacity=10, label='(9,10)', color='r')
+    # Add the edges
+    G.add_edge('A', 'B', cost=20, capacity=20)
+    G.add_edge('A', 'C', cost=7, capacity=15)
+    G.add_edge('B', 'E', cost=16, capacity=21)
+    G.add_edge('B', 'D', cost=4, capacity=4)
+    G.add_edge('C', 'D', cost=4, capacity=4)
+    G.add_edge('C', 'F', cost=3, capacity=9)
+    G.add_edge('D', 'E', cost=2, capacity=2)
+    G.add_edge('D', 'F', cost=6, capacity=10)
+    G.add_edge('E', 'G', cost=18, capacity=24)
+    G.add_edge('F', 'G', cost=9, capacity=10)
 
     return G
 
 
-def print_graph(G):
+def get_color_map(G):
     color_map = []
+
     for node in G:
         if node in {'G', 'A'}:  # Source and sink nodes
             color_map.append('red')
         else:
             color_map.append('blue')
+    return color_map
 
-    nx.draw(G, node_color=color_map, with_labels=True)
+
+def draw_graph(G):
+    color_map = get_color_map(G)
+    pos = nx.get_node_attributes(G, 'pos')
+    nx.draw(G, node_color=color_map, with_labels=True, pos=pos)
+    edge_labels = dict([((u, v,), "({},{})".format(
+        d['cost'], d['capacity']))for u, v, d in G.edges(data=True)])
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+
     plt.show()
     plt.close()
+
 
 def get_variables(G):
     variables = {}
@@ -50,6 +70,7 @@ def get_variables(G):
         variables[name] = LpVariable(
             name, 0, G[edge[0]][edge[1]]['capacity'], cat='Integer')
     return variables
+
 
 def get_constraints(variables):
     constraints = []
@@ -70,8 +91,9 @@ def get_constraints(variables):
     return constraints
 
 
-def get_objective(G,variables):
+def get_objective(G, variables):
     return lpSum(variables["{}-{}".format(edge[0], edge[1])] * G[edge[0]][edge[1]]['cost'] for edge in G.edges())
+
 
 def print_problem(prob):
     print("Status:{}".format(LpStatus[prob.status]))
